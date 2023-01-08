a0dfb74f7a73fdba710dbbeb9548fc44b7f68195
BluenightsBN <122196320+BluenightsBN@users.noreply.github.com>
2023-01-08 14:35:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var first = "Gauri";
+console.log("My first name is ",first);
+var second = "Mishra";
+console.log("My last name is ",second);
+
+var fullname = first.concat(second);
+console.log("My full name is ",fullname);
+
+var a = 50;
+console.log(a);
+var b = 2;
+console.log(b);
+
+var divide = a/b ;
+console.log("The quotient of these is ",divide);
